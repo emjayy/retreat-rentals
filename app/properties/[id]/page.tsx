752a3a02e74b99823b87ae1849b4af88b1ae0ad5
Martin Jones <martinjones@windowslive.com>
2024-11-02 11:30:14 +1300

    work in progress
    work in progress

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,6 +1,6 @@
 import FavouriteToggleButton from "@/components/card/FavouriteToggleButton";
 import BreadCrumbs from "@/components/properties/BreadCrumbs";
-import { fetchPropertyDetails } from "@/utils/actions";
+import { fetchPropertyDetails, findExistingReview } from "@/utils/actions";
 import { redirect } from "next/navigation";
 import ShareButton from "../ShareButton";
 import ImageContainer from "@/components/properties/ImageContainer";
@@ -13,6 +13,9 @@ import { Separator } from "@/components/ui/separator";
 import Amenities from "@/components/properties/Amenities";
 import dynamic from "next/dynamic";
 import { Skeleton } from "@/components/ui/skeleton";
+import SubmitReview from "@/components/reviews/SubmitReview";
+import PropertyReviews from "@/components/reviews/PropertyReviews";
+import { auth } from "@clerk/nextjs/server";
 
 const DynamicMap = dynamic(
   () => import("@/components/properties/PropertyMap"),
@@ -29,6 +32,12 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
   const details = { baths, bedrooms, beds, guests };
   const firstName = property.profile.firstName;
   const profileImage = property.profile.profileImage;
+
+  // check if current user has already submitted a review for a property, and is not the owner of the property:
+  const { userId } = auth();
+  const isNotOwner = property.profile.clerkId !== userId;
+  const reviewDoesNotExist =
+    userId && isNotOwner && !(await findExistingReview(userId, property.id));
 
   return (
     <section>
@@ -60,6 +69,8 @@ async function PropertyDetailsPage({ params }: { params: { id: string } }) {
           <BookingCalendar />
         </div>
       </section>
+      {reviewDoesNotExist && <SubmitReview propertyId={property.id} />}
+      <PropertyReviews propertyId={property.id} />
     </section>
   );
 }
